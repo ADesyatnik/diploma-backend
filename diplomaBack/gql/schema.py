cf3b60feb2f474b34aa934a39639538918de0ab0
Kirill Dolgikh <kirill.dolgikh@saritasa.com>
2020-06-04 15:30:47 +0700

    Get single company query.

--- diplomaBack/gql/schema.py
+++ diplomaBack/gql/schema.py
@@ -6,8 +6,15 @@ from diplomaBackend.models import Company, BusinessModel
 
 class Query(graphene.ObjectType):
     all_companies = graphene.List(CompanyType)
+    company = graphene.Field(CompanyType, id=graphene.Int())
 
     def resolve_all_companies(self, info, **kwargs):
         return Company.objects.all()
+
+    def resolve_company(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            return Company.objects.get(pk=id)
 
 schema = graphene.Schema(query=Query)
