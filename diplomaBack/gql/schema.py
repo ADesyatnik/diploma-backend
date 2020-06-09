5819c5963e92473eb1ba0fb6af056e0f35cd4609
Kirill Dolgikh <kirill.dolgikh@saritasa.com>
2020-06-09 12:08:11 +0700

    API for filters panel

--- diplomaBack/gql/schema.py
+++ diplomaBack/gql/schema.py
@@ -1,18 +1,27 @@
 import graphene
-from .types import CompanyNode
+from .types import (CompanyNode, BusinessModelNode,
+                    IndustryCategoryNode, PersonNode, ContactNode,
+                    StatusNode)
 from .resolvers import resolve_all_companies
-from diplomaBackend.models import Company
+from diplomaBackend.models import (
+    Company, BusinessModel, IndustryCategory, Person, Contact,
+    Status)
 from .mutations import CompanyMutation
 from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django.debug import DjangoDebug
 from graphql_auth import mutations
 from graphql_auth.schema import UserQuery, MeQuery
 from graphql import GraphQLError
+from django.db.models import Count
 
 
 class Query(UserQuery, MeQuery, graphene.ObjectType):
     all_companies = DjangoFilterConnectionField(CompanyNode)
     company = graphene.Field(CompanyNode, id=graphene.Int())
+    business_models = graphene.List(BusinessModelNode)
+    industry_categories = graphene.List(IndustryCategoryNode)
+    owners = graphene.List(ContactNode)
+    all_statuses = graphene.List(StatusNode)
 
     debug = graphene.Field(DjangoDebug, name='_debug')
 
@@ -31,6 +40,17 @@ class Query(UserQuery, MeQuery, graphene.ObjectType):
             else:
                 return Company.objects.get(pk=id)
 
+    def resolve_business_models(self, info, **kwargs):
+        return BusinessModel.objects.all()
+
+    def resolve_industry_categories(self, info, **kwargs):
+        return IndustryCategory.objects.all()
+
+    def resolve_owners(self, info, **kwargs):
+        return Contact.objects.exclude(owned_companies=None)
+
+    def resolve_all_statuses(self, info, **kwargs):
+        return Status.objects.all()
 
 class Mutation(graphene.ObjectType):
     create_company = CompanyMutation.Field()
@@ -44,7 +64,7 @@ class Mutation(graphene.ObjectType):
     archive_account = mutations.ArchiveAccount.Field()
     delete_account = mutations.DeleteAccount.Field()
     update_account = mutations.UpdateAccount.Field()
-    send_secondary_email_activation =  mutations.SendSecondaryEmailActivation.Field()
+    send_secondary_email_activation = mutations.SendSecondaryEmailActivation.Field()
     verify_secondary_email = mutations.VerifySecondaryEmail.Field()
     swap_emails = mutations.SwapEmails.Field()
 
